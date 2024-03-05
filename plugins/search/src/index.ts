80d60b65ffa2713344e158a8ed051fcfb67fec04
dubisdev <dubisdev@gmail.com>
2024-03-05 12:54:39 +0100

    fix: persistent no results item

--- plugins/search/src/index.ts
+++ plugins/search/src/index.ts
@@ -44,18 +44,22 @@ const run: ExtensionModule['run'] = async ({ term, actions, settings, display })
   return display([engineLauncherItem]);
 };
 
-const EngineSearchApp: ExtensionModule['run'] = async ({ term, display, actions, settings }) => {
+const EngineSearchApp: ExtensionModule['run'] = async (ctx) => {
+  const { term, display, hide, actions, settings } = ctx;
+
   const engine = settings['Search Engine'] as Engine;
   const icon = icons[engine] || defaultIcon;
   const handler = handlers[engine] || google;
   const { getSearchString, results } = await handler(term);
 
   const noResultsItem = new InfoItem({
+    id: 'no-results',
     title: `No results found for "${term}" on ${engine}.`,
     icon
   });
 
   if (!results.length) return display([noResultsItem]);
+  hide('no-results');
 
   const items = results.map(result => {
     return new ScriptItem({
