ba2547a67a8514f5a20826f95fb5567b44603cb3
dubisdev <dubisdev@gmail.com>
2024-03-09 13:57:55 +0100

    chore: separate app code from index

---
+++ extensions/search/src/EngineSearchApp.ts
@@ -0,0 +1,49 @@
+import { ExtensionModule, InfoItem, ScriptItem } from '@rokii/api';
+import {
+  google,
+  duckduckgo,
+  ecosia,
+  brave,
+  startpage
+} from './handlers';
+import { Engine, EngineHandler } from './types';
+import icons from './icons';
+
+const handlers: Record<Engine, EngineHandler> = {
+  Google: google,
+  DuckDuckGo: duckduckgo,
+  Ecosia: ecosia,
+  Brave: brave,
+  StartPage: startpage
+};
+
+export const EngineSearchAppName = 'EngineSearch';
+
+export const EngineSearchApp: ExtensionModule['run'] = async (ctx) => {
+  const { term, display, hide, actions, settings } = ctx;
+
+  const engine = settings['Search Engine'] as Engine;
+  const icon = icons[engine] || icons.Google;
+  const handler = handlers[engine] || google;
+  const { getSearchString, results } = await handler(term);
+
+  const noResultsItem = new InfoItem({
+    id: 'no-results',
+    title: `No results found for "${term}" on ${engine}`,
+    icon
+  });
+
+  if (!results.length) return display([noResultsItem]);
+  hide('no-results');
+
+  const items = results.map(result => {
+    return new ScriptItem({
+      icon,
+      title: result.title,
+      subtitle: result.description,
+      run: () => actions.open(getSearchString(result.title))
+    });
+  });
+
+  display(items);
+};
--- extensions/search/src/index.ts
+++ extensions/search/src/index.ts
@@ -1,22 +1,8 @@
-import { AppItem, ExtensionModule, InfoItem, ScriptItem } from '@rokii/api';
-import {
-  google,
-  duckduckgo,
-  ecosia,
-  brave,
-  startpage
-} from './handlers';
-import { Engine, EngineHandler } from './types';
+import { AppItem, ExtensionModule, ScriptItem } from '@rokii/api';
+import { Engine } from './types';
 import icons from './icons';
 import settings from './settings';
-
-const handlers: Record<Engine, EngineHandler> = {
-  Google: google,
-  DuckDuckGo: duckduckgo,
-  Ecosia: ecosia,
-  Brave: brave,
-  StartPage: startpage
-};
+import { EngineSearchApp, EngineSearchAppName } from './EngineSearchApp';
 
 const run: ExtensionModule['run'] = async ({ term, actions, settings, display }) => {
   if (!navigator.onLine) return;
@@ -29,12 +15,13 @@ const run: ExtensionModule['run'] = async ({ term, actions, settings, display })
     });
     return display([item]);
   }
+
   // handle built-in search engines
   const engine = settings['Search Engine'] as Engine;
   const engineIcon = icons[engine];
 
   const engineLauncherItem = new AppItem({
-    appName: 'EngineSearch',
+    appName: EngineSearchAppName,
     title: `Search ${term} with ${engine}`,
     icon: engineIcon
   });
@@ -42,40 +29,11 @@ const run: ExtensionModule['run'] = async ({ term, actions, settings, display })
   return display([engineLauncherItem]);
 };
 
-const EngineSearchApp: ExtensionModule['run'] = async (ctx) => {
-  const { term, display, hide, actions, settings } = ctx;
-
-  const engine = settings['Search Engine'] as Engine;
-  const icon = icons[engine] || icons.Google;
-  const handler = handlers[engine] || google;
-  const { getSearchString, results } = await handler(term);
-
-  const noResultsItem = new InfoItem({
-    id: 'no-results',
-    title: `No results found for "${term}" on ${engine}`,
-    icon
-  });
-
-  if (!results.length) return display([noResultsItem]);
-  hide('no-results');
-
-  const items = results.map(result => {
-    return new ScriptItem({
-      icon,
-      title: result.title,
-      subtitle: result.description,
-      run: () => actions.open(getSearchString(result.title))
-    });
-  });
-
-  display(items);
-};
-
 const SerachExtension: ExtensionModule = {
   name: 'Search',
   run,
   apps: {
-    EngineSearch: EngineSearchApp
+    [EngineSearchAppName]: EngineSearchApp
   },
   icon: icons.Google,
   settings
