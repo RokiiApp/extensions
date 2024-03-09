f09cc51f2e204cbf70090f520c6bd2ee6c271332
dubisdev <dubisdev@gmail.com>
2024-03-09 15:42:45 +0100

    chore: simplify anidation

--- extensions/math/src/index.ts
+++ extensions/math/src/index.ts
@@ -6,37 +6,39 @@ const run: ExtensionModule['run'] = async (ctx) => {
   const { term, display, actions } = ctx;
   const match = term.match(MATH_REGEXP);
 
-  if (match) {
-    try {
-      const calculateTerm = term.replace(/,/g, '.');
+  if (!match) return;
 
-      // eslint-disable-next-line no-eval
-      const result = eval(calculateTerm) as number;
+  try {
+    const calculateTerm = term.replace(/,/g, '.');
 
-      if (Number.isNaN(result)) {
-        const indeterminateItem = new ScriptItem({
-          title: term + ' = indeterminate',
-          run: () => {
-            actions.copyToClipboard('indeterminate');
-          }
-        });
-        // When user tries to devide 0 by 0
-        display([indeterminateItem]);
-        return;
-      }
-      const stringResult = result.toLocaleString();
+    // eslint-disable-next-line no-eval
+    const result = eval(calculateTerm);
 
-      const resultItem = new ScriptItem({
-        title: term + ' = ' + stringResult,
+    if (typeof result !== 'number') return;
+
+    if (Number.isNaN(result)) {
+      const indeterminateItem = new ScriptItem({
+        title: term + ' = indeterminate',
         run: () => {
-          actions.copyToClipboard(stringResult);
+          actions.copyToClipboard('indeterminate');
         }
       });
-
-      display([resultItem]);
-    } catch (err) {
-      // Do nothing when eval failed
+        // When user tries to devide 0 by 0
+      display([indeterminateItem]);
+      return;
     }
+    const stringResult = result.toLocaleString();
+
+    const resultItem = new ScriptItem({
+      title: term + ' = ' + stringResult,
+      run: () => {
+        actions.copyToClipboard(stringResult);
+      }
+    });
+
+    display([resultItem]);
+  } catch (err) {
+    // Do nothing when eval failed
   }
 };
 
